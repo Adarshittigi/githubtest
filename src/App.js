312996b0e9e1737974a27a7fc44b03055295033a
Adarsh <adarsh.ittigi@gmail.com>
2022-01-28 15:08:36 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Mycomponents/header";
+import { Todos } from './Mycomponents/Todos';
+import { Todo } from './Mycomponents/Todo';
+import { Footer } from './Mycomponents/Footer';
 
 function App() {
+    let myVariable = 345;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Todos/>
+    <Todo/>
+    <Footer/>
+    </>
   );
 }
 
